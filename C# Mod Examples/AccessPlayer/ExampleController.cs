1d37de8a0d512e4ebd3b625b87027b7b9edf12d4
Zachary Braden <103508025+kobrakon@users.noreply.github.com>
2022-08-18 13:17:40 -0400

    Update ExampleController.cs

--- C# Mod Examples/AccessPlayer/ExampleController.cs
+++ C# Mod Examples/AccessPlayer/ExampleController.cs
@@ -4,6 +4,8 @@ using Comfort.Common;
 
 namespace AccessPlayerExample
 {
+    // Here I'll showcase how to access the player from both the internal player list
+    // and how to access the player's GameObject
     public class AccessPlayerController : MonoBehaviour
     {
         private static GameWorld gameWorld;
@@ -18,13 +20,22 @@ namespace AccessPlayerExample
                 return;
             }
 
-            // add logic here
+            var gamePlayer = gameWorld.AllPlayers[0];
+            
+            // And from here, just add a dot to the end of gamePlayer var or whatever you named it and from there you can explore
+            // all the things you can modify without patching
+            
+            player = GameObject.Find("PlayerSuperior(clone)"); // When finding gameobject the tag has to be exact, so it can be pretty hit or miss
+            // use UnityExplorer to find GameObject's names
+            
+            // Now the player field will have the GameObject for the player, which will allow you to configure
+            // add or destroy components attached to the player
         }
 
-        bool Ready() // bool method to ensure world is loaded
-        {   // checks that the gameworld and player list isn't null
-            // if you dont want you script to run if the player is in the hideout
-            // then add '|| gameWorld.AllPlayers[0] is HideoutPlayer'to the far right of the if condition
+        bool Ready() // Bool method to ensure world is loaded
+        {   // Checks that the gameworld and player list isn't null
+            // If you dont want you script to run if the player is in the hideout
+            // then add '|| gameWorld.AllPlayers[0] is HideoutPlayer' to the far right of the if condition
             if (gameWorld == null || gameWorld.AllPlayers == null || gameWorld.AllPlayers.Count <= 0) return false;
             else return true;
         }
